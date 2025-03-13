cc9d1b1b7c1f6816948381c29422da8d80cd026d
Frans Karlsson <frakar-4@student.ltu.se>
2025-03-13 11:23:37 +0100

    removed remnants of merging

--- labb6/src/labb6/simulator/SimState.java
+++ labb6/src/labb6/simulator/SimState.java
@@ -6,13 +6,11 @@ import java.util.Observable;
  * The central state of the simulator
  */
 public class SimState extends Observable {
-<<<<<<< HEAD
 	
 	/**
 	 * Notify all observers that an event is finished and the state can be read
 	 */
-	public void eventFinished() {
-=======
+	public void eventFinished() {}
 
 	private boolean runs = true;
 
@@ -25,7 +23,6 @@ public class SimState extends Observable {
 	}
 
 	public void eventFinished(Event event) {
->>>>>>> branch 'main' of git@github.com:frakar4/labb6.git
 		setChanged();
 		notifyObservers(event);
 	}
